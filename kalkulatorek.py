c9d77d40470c629bc1bb7c68e2d035d26f9d9c2a
rokoz1 <131867001+rokoz1@users.noreply.github.com>
2023-04-26 12:02:28 +0200

    Add files via upload

---
+++ kalkulatorek.py
@@ -0,0 +1,32 @@
+while True:
+    print("aby użyć kalkulatora wciśnij \'k\'")
+    print('aby zakończyć matematyczną przygodę wciśnij \'f\'')
+    decyzja = input()
+    if decyzja == 'k':
+        print('ahoj kamracie, jakie liczby masz dziś w głowie?')
+        print('powiedz pierwszą')
+        liczba1 = int(input())
+        print('ok. ok. nadążam.\n','podaj drugą')
+        liczba2= int(input())
+        print('a co byś chciał?')
+        odp=input()
+        if odp == 'dodawanie':
+            wynik1=liczba1+liczba2
+            print('wynikiem dodawania będzie:', wynik1)
+        elif odp == 'odejmowanie':
+            wynik2=liczba1-liczba2
+            wynik3=liczba1-liczba2
+            print('wynikiem odejmowania będzie:', wynik2, "lub:", wynik3)
+        elif odp == 'mnożenie':
+            wynik4=liczba1*liczba2
+            print('ok, wynikiem mnożenia będzie"', wynik4)
+        elif odp == 'dzielenie':
+            wynik5=liczba1/liczba2
+            print('wynikiem tego dzielenia będzie:', wynik5)
+        else:
+            print('sora, ale nie znam takiego działania')
+    elif decyzja == 'f':
+        break
+    else:
+        print('niunia, szanujmy się, nie takie były opcje wyboru')
+        print('dam ci kolejną szansę')
